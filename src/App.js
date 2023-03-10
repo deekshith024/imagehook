a88144d461e091b3304fd2aed7d786e68fb34925
deekshith024 <105255904+deekshith024@users.noreply.github.com>
2023-03-10 14:32:28 -0500

    onClick

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
- import React from "react";
- import "./App.css";
-  const App = () => {
-    const url = "https://images.unsplash.com/photo-1562051036-e0eea191d42f";
-    //  const displayImage = false;
-     const [displayImage, setState] = React.useState(false);
-     return (
-     <div className="App">
-      {displayImage && <img src={url} alt="Some coffee beans" />}
-       <br />
-       <button>Toggle Image Display</button>
-     </div>
-     );
-   };
- export default App;
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  const url = "https://images.unsplash.com/photo-1562051036-e0eea191d42f";
+  const [displayImage, setState] = React.useState(false);
+  const toggleImage = () => setState(!displayImage);
+  return (
+  <div className="App">
+    {displayImage && <img src={url} alt="Some coffee beans" />}
+    <br />
+    <button onClick={toggleImage}>Toggle Image Display</button>
+  </div>
+  );
+};
+
+export default App;
