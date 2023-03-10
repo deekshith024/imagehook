e3a3d2f02f0a5c32271f1f85575c89f34be3897a
deekshith024 <105255904+deekshith024@users.noreply.github.com>
2023-03-10 14:29:12 -0500

    toggle image button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+ import React from "react";
+ import "./App.css";
+  const App = () => {
+    const url = "https://images.unsplash.com/photo-1562051036-e0eea191d42f";
+    //  const displayImage = false;
+     const [displayImage, setState] = React.useState(false);
+     return (
+     <div className="App">
+      {displayImage && <img src={url} alt="Some coffee beans" />}
+       <br />
+       <button>Toggle Image Display</button>
+     </div>
+     );
+   };
+ export default App;
